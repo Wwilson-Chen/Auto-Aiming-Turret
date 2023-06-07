e56cba6e98c1cfeda9f5e2cc76aa0391a03567ca
Wwilson-Chen <73149623+Wwilson-Chen@users.noreply.github.com>
2023-06-07 13:47:21 +0800

    Add files via upload

--- PID.ino
+++ PID.ino
@@ -14,9 +14,6 @@ unsigned long firing_start_time = 0;
 unsigned long firing_current_time = 0;
 const long firing_time = 150; //射擊時間
 
-const double xcenter = 176;
-const double ycenter = 70;
-
 
 // unsigned long recoil_start_time = 0;
 // unsigned long recoil_current_time = 0;
@@ -30,16 +27,23 @@ const int shoot = 5; //開槍馬達腳位
 const int stop = 6; //停止提升子彈馬達腳位
 
 double Kp=2, Ki=5, Kd=1; //調PID參數
-PID pidX(&inputX, &outputX, &setpointX, Kp, Ki, Kd, DIRECT);
-PID pidY(&inputY, &outputY, &setpointY, Kp, Ki, Kd, DIRECT);
+float errorX = 0, PX = 0, IX = 0, DX = 0, PID_valueX = 0;//pid直道参数
+float errorY = 0, PY = 0, IY = 0, DY = 0, PID_valueY = 0;//pid直道参数
+float previous_errorX = 0, previous_errorY = 0, previous_I = 0;           //误差值
+
+// PID pidX(&inputX, &outputX, &setpointX, Kp, Ki, Kd, DIRECT);
+// PID pidY(&inputY, &outputY, &setpointY, Kp, Ki, Kd, DIRECT);
 
 void setup() {
   Serial.begin(9600); // Initialize the hardware serial port for debugging
   mySerial.begin(115200); // Initialize the software serial port
 
-  servoX.attach(7); //馬達X腳位
-  servoY.attach(8); //馬達Y腳位
+  servoX.attach(10); //馬達X腳位
+  servoY.attach(9); //馬達Y腳位
 
+  servoX.write(176);
+  servoY.write(70);
+  
   pinMode(shoot, OUTPUT);
   pinMode(stop, OUTPUT);
 
@@ -56,11 +60,8 @@ void setup() {
   int Y_low = 235;
 
   //啟動PID控制器
-  pidX.SetMode(AUTOMATIC);
-  pidY.SetMode(AUTOMATIC);
-
-  writeServoX(xcenter);
-  writeServoY(ycenter);
+  // pidX.SetMode(AUTOMATIC);
+  // pidY.SetMode(AUTOMATIC);
 }
 
 void loop() {
@@ -78,7 +79,13 @@ void loop() {
     int inputY = strB.toInt(); //讀取紅心Y座標
     Serial.println(inputX);
     Serial.println(inputY);
+    printf(inputX);
+    printf(inputY);
+    errorX = inputX - setpointX;
+    errorY = inputY - setpointY;
+    
   }
+  printf("123");
 
   if (inputX<600 && inputX>200 && inputY<500 && inputY>100){
     can_fire = true;
@@ -89,11 +96,37 @@ void loop() {
     can_fire = false;
   }
 
-  pidX.Compute();
-  pidY.Compute();
+  // pidX.Compute();
+  // pidY.Compute();
+  //calc_pidX();
+  //calc_pidY();
   
-  writeServoX(outputX);
-  writeServoY(outputY);
+  PX = errorX;
+  IX = IX + errorX;
+  DX = errorX - previous_errorX;
+  PID_valueX = (Kp * PX) + (Ki * IX) + (Kd * DX);
+  previous_errorX = errorX;
+  Serial.println(errorX);
+  errorX=0;
+  PY = errorY;
+  IY = IY + errorY;
+  DY = errorY - previous_errorY;
+  PID_valueY = (Kp * PY) + (Ki * IY) + (Kd * DY);
+  previous_errorY = errorY;
+  Serial.println(errorY);
+  errorY=0;
+  //writeServoX();
+  //writeServoY();
+
+  int errorX = map(PID_valueX, -800, 800, 86, 266); 
+  errorX = errorX + inputX;
+  Serial.println(errorX);
+  servoX.write(errorX);
+  int errorY = map(PID_valueY, -600, 600, 10, 100); 
+  errorY = errorY + inputY;
+  Serial.println(errorY);
+  servoY.write(errorY);
+
 }
 
 
@@ -107,14 +140,33 @@ void loop() {
 //   return ;
 // }
 
+void calc_pidX()
+{
+  PX = errorX;
+  IX = IX + errorX;
+  DX = errorX - previous_errorX;
+  PID_valueX = (Kp * PX) + (Ki * IX) + (Kd * DX);
+  previous_errorX = errorX;
+}
 
-void writeServoX(double value) {
-  int angleX = map(outputX, -255, 255, xcenter - 60, xcenter + 60); 
+void calc_pidY()
+{
+  PY = errorY;
+  IY = IY + errorY;
+  DY = errorY - previous_errorY;
+  PID_valueY = (Kp * PY) + (Ki * IY) + (Kd * DY);
+  previous_errorY = errorY;
+}
+
+void writeServoX() {
+  int angleX = map(PID_valueX, -800, 800, 86, 266); 
+  angleX = angleX + inputX;
   servoX.write(angleX);
 }
 
-void writeServoY(double value) {
-  int angleY = map(outputY, -255, 255, ycenter - 60, ycenter + 60); 
+void writeServoY() {
+  int angleY = map(PID_valueY, -600, 600, 10, 100); 
+  angleY = angleY + inputY;
   servoY.write(angleY);
 }
 
